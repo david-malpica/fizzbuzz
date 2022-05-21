ccf002b88457770d2b53015d211d881f3d54e35a
David Malpica <dmalpica96@hotmail.com>
2022-05-20 19:55:16 -0500

    Test Github actions

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,7 +1,7 @@
 const ExplorerService = require('./../../lib/services/ExplorerService')
 
 describe("Test for ExplorerService", () => {
-    test ("", () => {
+    test ("Test ExplorerService class", () => {
         const explorers = [{mission: 'node'}]
         const explorersInNode = ExplorerService.filterByMission(explorers,'node')
         expect(explorersInNode.length).toBe(1)
